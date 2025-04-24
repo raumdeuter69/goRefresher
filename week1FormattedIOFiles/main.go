ec0352d456c6c2069bba8f35ce9bbf6167af7e8b
sameerkun-sureify <surya.kuntamukkala@sureify.com>
2025-04-24 11:24:30 +0530

    Updated Homework

--- week1FormattedIOFiles/main.go
+++ week1FormattedIOFiles/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 
 	for _, fName := range os.Args[1:] {
-		var wordsNumber, sizeOftheFile int
+		var wordsNumber, sizeOftheFile, totalFiles, totalWords, totalSize int
 		fs, err := os.Open(fName)
 		if err != nil {
 			fmt.Println(err)
@@ -24,8 +24,14 @@ func main() {
 			wordsNumber += len(strings.Split(text, " "))
 		}
 		fmt.Println(wordsNumber, sizeOftheFile, fName)
+		totalWords += wordsNumber
+		totalSize += sizeOftheFile
 		sizeOftheFile = 0
 		wordsNumber = 0
 		fs.Close()
+		totalFiles += 1
+		if totalFiles > 1 {
+			fmt.Println(totalWords, totalSize, totalFiles)
+		}
 	}
 }
